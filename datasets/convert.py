db49562bc104c2e11c28a421ac142eb602f6a07f
mvmfaria <marcosfaria03@hotmail.com>
2026-05-18 15:54:53 -0300

    add argument to convert script for symlinking images

--- datasets/convert.py
+++ datasets/convert.py
@@ -1,22 +1,36 @@
 from ultralytics.data.converter import convert_coco
 import argparse
+import os
 
 def main():
-    parser = argparse.ArgumentParser() 
+    parser = argparse.ArgumentParser()
+
     parser.add_argument(
         "--source",
         type=str,
         required=True,
     )
-    
+
+    parser.add_argument(
+        "--symlink-images-from",
+        type=str,
+    )
+
     args = parser.parse_args()
-    
+
     convert_coco(
-        labels_dir=f"datasets/piglife/coco/{args.source}/annotations/", 
+        labels_dir=f"datasets/piglife/coco/{args.source}/annotations/",
         save_dir=f"datasets/piglife/yolo/{args.source}/",
         use_segments=False,
         cls91to80=False
     )
+
+    if args.symlink_images_from:
+        target = os.path.abspath(f"datasets/piglife/yolo/{args.symlink_images_from}/images")
+        link = f"datasets/piglife/yolo/{args.source}/images"
+        if os.path.isdir(link) and not os.path.islink(link):
+            os.rmdir(link)
+        os.symlink(target, link)
 
 if __name__ == "__main__":
     main()
